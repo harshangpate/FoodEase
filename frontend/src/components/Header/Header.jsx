c867d10e552dba22142d736ceb12fc26fd309d25
Harshang Patel <harshangpatelh@gmail.com>
2025-09-17 17:16:55 +0530

    header changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Header.css'
+import headerImage from '../../assets/header_img.jpg'
 
 const Header = () => {
     return (
-        <div className='header'>
+        <div className='header' style={{ backgroundImage: `url(${headerImage})` }}>
             <div className='header-contents'>
                 <h2>Order your favourite food here</h2>
                 <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
